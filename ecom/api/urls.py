e5f2af8e99d91bd87e15711b56647b8b2e35fae9
Rokon Ron <rokon.raz@gmail.com>
2023-08-02 12:15:46 +0600

    Add a path in api urls.

--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -5,6 +5,8 @@ from .views import home
 
 
 urlpatterns = [
-    path('', home, name= 'api.home')
+    path('', home, name= 'api.home'),
+    path('category/', include('api.category.urls'))
+   
 ]
 
