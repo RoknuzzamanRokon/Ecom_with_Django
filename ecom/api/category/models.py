5f29db029070ff6ccb9b611f9f2ad9b524dc43c0
Rokon Ron <rokon.raz@gmail.com>
2023-08-02 09:23:31 +0600

    Edit admin file,app file,modeles file.

--- ecom/api/category/models.py
+++ ecom/api/category/models.py
@@ -8,3 +8,14 @@ class Category(models.Model):
     crated_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
+    
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
