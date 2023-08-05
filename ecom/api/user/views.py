b7365dc16da167f0cc1c3c6a97be2dde2705fcd2
Rokon Ron <rokon.raz@gmail.com>
2023-08-06 02:03:58 +0600

    edit views.py file

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -1,9 +1,9 @@
-# from rest_framework import viewsets
-# from .serializers import UserSerializer
-# from .models import CustomUser
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from .models import CustomUser
 
 
-# # Create your views here.
-# class UserViewsSet(viewsets.ModelViewSet):
-#     queryset = CustomUser.objects.all().order_by('something')
-#     serializer_class = UserSerializer
+# Create your views here.
+class UserViewsSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all().order_by('something')
+    serializer_class = UserSerializer
