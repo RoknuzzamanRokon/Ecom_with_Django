758392cbeb7d725f262888ec615f52b2e86d239f
Rokon Ron <rokon.raz@gmail.com>
2023-08-05 15:47:56 +0600

    Edit views.py file. import some fuction in product folder.import viewsets from rest_framework and create a new class and create a queryset variable and pass id here and also call ProductSerializer function.

--- ecom/api/product/views.py
+++ ecom/api/product/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .serializers import ProductSerializer
+from .models import Product
+from rest_framework import viewsets
 
 # Create your views here.
+class ProductViewsSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('id')
+    serializer_class = ProductSerializer
+
+
+
+
