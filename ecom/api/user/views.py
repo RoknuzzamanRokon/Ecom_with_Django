364d747b4e8c6bb479441b2258f2f1c23f51f5c9
Rokon Ron <rokon.raz@gmail.com>
2023-08-06 02:14:39 +0600

    create a function randome  10 letter generator.

--- ecom/api/user/views.py
+++ ecom/api/user/views.py
@@ -2,6 +2,13 @@ from rest_framework import viewsets
 from .serializers import UserSerializer
 from .models import CustomUser
 
+# Generate a Random number.
+import random
+
+def generate_session_token(length=10):
+    return ''.join(random.SystemRandom().choice([chr(i) for i in range(97,123)] + [str(i) for i in range(10)]) for _ in range(length))
+
+
 
 # Create your views here.
 class UserViewsSet(viewsets.ModelViewSet):
