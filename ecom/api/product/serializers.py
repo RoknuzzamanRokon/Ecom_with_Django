1181bdf4d7b544f20bdd1d08cdbe01d15a96a61b
Rokon Ron <rokon.raz@gmail.com>
2023-08-05 15:13:49 +0600

    Add image file in serializers file.

--- ecom/api/product/serializers.py
+++ ecom/api/product/serializers.py
@@ -3,7 +3,7 @@ from .models import Product
 
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
-    
+    image = serializers.ImageField(max_length = None, allow_empty_file = False, allow_null = True, required= False)
     class meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'stock', 'image', 'category' )
