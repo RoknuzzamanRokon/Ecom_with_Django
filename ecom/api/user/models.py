01593c381194ec6fd95095029dbe5784fe83540d
Rokon Ron <rokon.raz@gmail.com>
2023-08-08 17:53:06 +0600

    Create custom user.

--- ecom/api/user/models.py
+++ ecom/api/user/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+    """User models"""
     name = models.CharField(max_length=50, default = 'Anonymous')
     email = models.EmailField(max_length=250,unique=True)
     phone = models.CharField(max_length=20, blank=True, null=True)
