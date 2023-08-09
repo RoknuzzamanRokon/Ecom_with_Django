23cfc3f18a7781da316e7933a629dde83d28f51a
Rokon Ron <rokon.raz@gmail.com>
2023-08-09 11:52:11 +0600

    create order model.

--- ecom/api/order/models.py
+++ ecom/api/order/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from api.user.models import CustomUser
+from api.product.models import Product
 
 # Create your models here.
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True)
+    product_name = models.CharField(max_length=250)
+    product_description = models.CharField(max_length=250)
+    product_price = models.CharField(max_length=50)
+    product_stock = models.CharField(max_length=500)
+    transaction_id = models.CharField(max_length=50)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
